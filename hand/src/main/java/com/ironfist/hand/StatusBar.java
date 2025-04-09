15211b82ff47f1e9bc8800d589e49b7f5d28bef6
irunfist <irunfist@gmail.com>
2025-04-09 20:41:23 +0530

    nothing

---
+++ hand/src/main/java/com/ironfist/hand/StatusBar.java
@@ -0,0 +1,38 @@
+package com.ironfist.hand;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowInsetsController;
+
+public class StatusBar {
+    public static void setStatusBar(Window window) {
+        // Common reference for window configuration
+        if (window != null) {
+            // Navigation Bar Configuration
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                WindowInsetsController insetsController = window.getInsetsController();
+                if (insetsController != null) {
+                    insetsController.setSystemBarsAppearance(
+                            WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS,
+                            WindowInsetsController.APPEARANCE_LIGHT_NAVIGATION_BARS
+                    );
+                    insetsController.setSystemBarsAppearance(
+                            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                            WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+                    );
+                }
+            } else {
+                window.getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR |
+                                View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+                );
+                window.setNavigationBarColor(Color.parseColor("#FFFFFF"));
+                window.setStatusBarColor(Color.WHITE);
+            }
+        }
+    }
+}
